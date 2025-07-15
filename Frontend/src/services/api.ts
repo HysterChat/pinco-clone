79939992ed6b2109a04e211ecacd199278e81f8a
charan-crafts <mangalacharancharan@gmail.com>
2025-07-15 17:17:26 +0530

    Updates

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = 'https://eval8 aiclone.hysterchat.com/api';
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = 'https://eval8 aiclone.hysterchat.com/api';
+// const BASE_URL = 'http://localhost:8000/api';
 
 
 // Create axios instance
